1e2b7765eae483c89f13757cb1d94a4c80c8bf99
hAhmadz <haaris.ahmad1@gmail.com>
2017-03-19 15:26:35 +1100

    Initial Setup

---
+++ ProjectA/src/projecta/NimPlayer.java
@@ -0,0 +1,43 @@
+//Player Class
+
+package projecta;
+public class NimPlayer 
+{
+    private String playerName;
+    
+    
+    // *** constructors ***
+    NimPlayer()
+    {
+        playerName = "";
+    }
+    
+    NimPlayer(String name)
+    {
+        playerName = name;
+    }
+    
+    //*** End Constructors ***
+
+    
+    // *** Functions ***
+    public String getPlayerName() {
+        return playerName;
+    }
+    
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void removeStone()
+    {
+        
+        //return stones;
+    }
+    
+    // *** End Functions ***
+
+    
+
+    
+}
--- ProjectA/src/projecta/Nimsys.java
+++ ProjectA/src/projecta/Nimsys.java
@@ -1,8 +1,67 @@
 package projecta;
+import java.util.Scanner;
+
 public class Nimsys 
 {
     public static void main(String[] args) 
     {
-        String s = new String();
+        //all variables
+        Scanner scan = new Scanner(System.in);
+        Boolean gameCheck = true;
+        int upperBound;
+        int stones;
+        int turnInput;
+        NimPlayer player1;
+        NimPlayer player2;
+        
+        System.out.println("Welcome to Nim");
+        System.out.println("Please enter Player 1's name");
+        player1 = new NimPlayer(scan.next());
+        
+        System.out.println("Please enter Player 2's name");
+        player2 = new NimPlayer(scan.next());
+        
+        
+        if(gameCheck)
+        {
+            System.out.println("Please enter upper bound of stone removal");
+            upperBound = scan.nextInt();
+
+            System.out.println("Please enter initial number of stones");
+            stones = scan.nextInt();
+
+            System.out.print(stones + " stones left: ");
+
+
+            //Main game Loop
+            for(int i=0;i<stones;i++)
+                System.out.print('*');
+
+            System.out.println(); //blank line
+            System.out.println(player1.getPlayerName() + "'s turn - remove how many?");
+            turnInput = scan.nextInt();
+
+            for(int i=0;i<stones;i++)
+                System.out.print('*');
+
+            System.out.println(); //blank line
+            System.out.println(player2.getPlayerName() + "'s turn - remove how many?");
+            turnInput = scan.nextInt();
+
+
+
+            System.out.println("Game Over");
+            //System.out.println( + " Wins"); //player wins
+            
+            System.out.print("Do you want to play again (Y/N): ");
+            if(scan.next().toUpperCase().equals('Y'))
+                gameCheck = true;
+            else 
+                gameCheck = false;
+                
+        
+        }
+        
+        
     }
 }
